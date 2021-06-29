dfcafcb5ad437189ee91d08201ffa41b2a3099ed
Faris Han <farishanugraha@gmail.com>
2021-06-29 23:13:32 +0700

    Update exampleWithRouter

--- examples/exampleWithRouter/terminal.js
+++ examples/exampleWithRouter/terminal.js
@@ -1,10 +1,18 @@
 const terminal = {
+  $root: document.body,
   node: {
     root: document.createElement('form'),
     input: document.createElement('input'),
   },
 
   onchange: null,
+
+  emitEventTargets: [/* list of nodes goes here */],
+  emitEvent: 'terminalEmit',
+
+  addEmitEventTarget: function(node){
+    this.emitEventTargets.push(node)
+  },
 
   init: function(){
     // Setup node attributes
@@ -22,6 +30,10 @@ const terminal = {
 
     // Assemblages
     this.node.root.appendChild(this.node.input);
+
+    this.$root.appendChild(this.node.root);
+
+    this.finalize()
   },
 
   // Method to execute after terminal appended to document.body
@@ -34,6 +46,18 @@ const terminal = {
   emit: function(payload){
     if(this.onchange !== null && typeof this.onchange === 'function'){
       this.onchange(payload)
+    }
+
+    if(this.emitEventTargets.length > 0){
+      const event = new CustomEvent('terminalEmit', { detail: payload });
+
+      this.emitEventTargets.forEach(target => {
+        // Listen for the event.
+        // target.addEventListener('terminalEmit', function (e) { /* ... */ }, false);
+
+        // Dispatch the event.
+        target.dispatchEvent(event);
+      });
     }
   },
 
--- examples/exampleWithRouter/ui.js
+++ examples/exampleWithRouter/ui.js
@@ -1,10 +1,122 @@
 const $root = document.body;
 
+const itemManager = {
+  itemMap: {
+    chip: window.chip,
+    core: window.core
+  },
+
+  create: function(item){
+    if (this.itemMap[item]) {
+      // Create item object
+      const itemObject = new this.itemMap[item]();
+
+      // Test object
+      if (itemObject.speak) {
+        itemObject.speak();
+      }
+
+      // Create item node
+      const itemNode = document.createElement("div");
+
+      // Merge object with node
+      itemNode.id = itemObject.id;
+      itemNode.dataset.props = JSON.stringify(itemObject);
+
+      // Node styling
+      itemNode.setAttribute(
+        "style",
+        `
+        border: 1px solid;
+        display: inline-block;
+        padding: 5px;
+      `
+      );
+      itemNode.style.border = "1px solid";
+      itemNode.style.display = "inline-block";
+      const text = document.createElement("div");
+      text.innerHTML = `${itemNode.id} | ${item}`;
+      itemNode.appendChild(text);
+
+      return {
+        itemObject,
+        itemNode
+      }
+
+      // $root.appendChild(itemNode);
+    }
+  },
+
+  join: function(arg1, arg2){
+    let item1, item2;
+
+    if (arg1 && el(arg1)) {
+      item1 = el(arg1);
+    }
+
+    if (arg2 && el(arg2)) {
+      item2 = el(arg2);
+    }
+
+    if (item1 && item2) {
+      item1.appendChild(item2.cloneNode(true));
+      item2.remove();
+    }
+  }
+}
+
+const terminalItemManagerConnector = {
+  scopes: ['/garage'],
+  terminal: null,
+  terminalListener: document.createElement('div'),
+  itemManager: null,
+
+  commandMap: {
+    "create": function(item){
+      const {itemObject, itemNode} = itemManager.create(item)
+
+      console.log(itemObject)
+
+      if (itemNode) $root.appendChild(itemNode);
+    },
+    "join": function(arg1, arg2){
+      itemManager.join(arg1, arg2)
+    }
+  },
+
+  init: function(terminal, itemManager){
+    this.terminal = terminal
+    this.itemManager = itemManager
+
+    this.terminalListener.addEventListener(this.terminal.emitEvent, (payload) => this.handleTerminal(payload))
+    this.terminal.addEmitEventTarget(this.terminalListener)
+  },
+  handleTerminal: function(event){
+    if(this.scopes.includes(this.terminal.node.input.dataset.scope)){
+      const payload = event.detail
+
+      const word1 = payload[0];
+      const word2 = payload[1];
+      const word3 = payload[2];
+
+      if (word1 && word1.length > 0) {
+        this.handleCommand(word1, word2, word3);
+      }
+    }
+  },
+  handleCommand: function(command, arg1, arg2){
+    if(this.commandMap[command]){
+      this.commandMap[command](arg1, arg2)
+    } else {
+      console.log(`"${command}" is not a item manager command.`);
+    }
+  }
+}
+
 const terminalRouterConnector = {
   terminal: null,
+  terminalListener: document.createElement('div'),
   router: null,
-
-  tags: ["chip", "core"],
 
   commandMap: {
     "to": function(newRoute){
@@ -12,61 +124,6 @@ const terminalRouterConnector = {
       self.router.push(newRoute)
       self.terminal.node.input.dataset.scope = self.router.currentPath
     },
-    "create": function(tag){
-      const self = terminalRouterConnector
-      console.log("create something...", tag);
-
-      if (tag.length > 0) {
-        if (self.tags.includes(tag)) {
-          console.log({ tag });
-          const element = document.createElement("div");
-
-          if (window[tag]) {
-            // Create item object
-            const item = new window[tag]();
-
-            if (item.speak) {
-              item.speak();
-            }
-
-            element.id = item.id;
-            element.dataset.props = JSON.stringify(item);
-            element.setAttribute(
-              "style",
-              `
-              border: 1px solid;
-              display: inline-block;
-              padding: 5px;
-            `
-            );
-            element.style.border = "1px solid";
-            element.style.display = "inline-block";
-            const text = document.createElement("div");
-            text.innerHTML = `${element.id} | ${tag}`;
-            element.appendChild(text);
-            $root.appendChild(element);
-          }
-        }
-      }
-    },
-    "join": function(arg1, arg2){
-      console.log(arg1, arg2);
-
-      let item1, item2;
-
-      if (arg1 && el(arg1)) {
-        item1 = el(arg1);
-      }
-
-      if (arg2 && el(arg2)) {
-        item2 = el(arg2);
-      }
-
-      if (item1 && item2) {
-        item1.appendChild(item2.cloneNode(true));
-        item2.remove();
-      }
-    }
   },
 
   commandMapByRoute: {
@@ -79,20 +136,22 @@ const terminalRouterConnector = {
     this.router = router
 
     this.terminal.node.input.dataset.scope = this.router.currentPath
-    this.terminal.onchange = (payload) => this.handleTerminal(payload)
+
+    this.terminalListener.addEventListener(this.terminal.emitEvent, (payload) => this.handleTerminal(payload))
+    this.terminal.addEmitEventTarget(this.terminalListener)
 
     // this.router.onchange = (payload) => this.handleRouter(payload)
   },
-  handleTerminal: function(payload) {
-    console.log(payload, this)
+  handleTerminal: function(event) {
+    const payload = event.detail
+
     let commandMapByRouteKey = this.router.currentPath
     const word1 = payload[0];
     const word2 = payload[1];
     const word3 = payload[2];
 
     if(word1.charAt(0) === '/'){
-      this.router.push(word1)
-      commandMapByRouteKey = this.router.currentPath
+      this.commandMap["to"](word1)
     }
 
     if (word1 && word1.length > 0) {
@@ -106,7 +165,7 @@ const terminalRouterConnector = {
       if(this.commandMap[command]){
         this.commandMap[command](arg1, arg2)
       } else {
-        console.log(`"${command}" is not valid.`);
+        console.log(`"${command}" is not a router command.`);
       }
     } else {
       console.log(`"${command}" is not valid for current route. Valid commands: ${JSON.stringify(commandsByRoute)}.`)
@@ -115,6 +174,4 @@ const terminalRouterConnector = {
 }
 
 terminalRouterConnector.init(terminal, router)
-
-$root.appendChild(terminal.node.root);
-terminal.finalize()
+terminalItemManagerConnector.init(terminal, itemManager)
